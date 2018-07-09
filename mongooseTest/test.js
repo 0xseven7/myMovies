1711fc2fc63fda5e487c8c2125856b2e50c8f5d2
zhangchenxu <zhangxu1161@gmail.com>
2018-07-09 23:04:25 +0800

    添加comment数据模型
    commnet已添加到数据库中

--- mongooseTest/test.js
+++
@@ -1,9 +0,0 @@
-let mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
-
-let testSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  sex: String
-});
-module.exports = mongoose.model('Test', testSchema);
---
+++ schemas/commentSchema.js
@@ -0,0 +1,42 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let ObjId = Schema.Types.ObjectId;
+let MovieSchema = new mongoose.Schema({
+  movie: {
+    type: ObjId,
+    ref: 'Movie'
+  },
+  from: {
+    type: ObjId,
+    ref: 'User'
+  },
+  to: {
+    type: ObjId,
+    ref: 'User'
+  },
+  content: String
+
+});
+// MovieSchema.pre('save', function (next) {
+//   if (this.isNew) {
+//     this.meta.createAt = this.meta.updateAt = Date.now();
+//   } else {
+//     this.meta.updateAt = Date.now();
+//   }
+// });
+MovieSchema.statics = {
+  fetch: function (cb) {
+    return this
+      .find({})
+      .sort('meta.updateAt')
+      .exec(cb);
+  },
+  findById: function (id, cb) {
+    return this
+      .findOne({_id: id})
+      .exec(cb);
+  }
+};
+
+module.exports = mongoose.model('Movie', MovieSchema);
