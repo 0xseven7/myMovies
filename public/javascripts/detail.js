950289b47983a92215f90ee886f26d09fb196bff
zhangchenxu <zhangxu1161@gmail.com>
2018-07-14 17:47:07 +0800

    comments compelete

--- public/javascripts/detail.js
+++ public/javascripts/detail.js
@@ -7,17 +7,25 @@ $(function () {
     console.log(commentId);
     // 动态插入隐藏域
 
-    $('<input>').attr({
-      type: 'hidden',
-      name: 'comment[tid]',
-      value: toId
-    }).appendTo('#commentForm');
-
-    $('<input>').attr({
-      type: 'hidden',
-      name: 'comment[cid]',
-      value: commentId,
-    }).appendTo('#commentForm')
-
+    if ($('#toId').length > 0) {
+       $('#toId').val(toId)
+    }else {
+      $('<input>').attr({
+        type: 'hidden',
+        name: 'comment[tid]',
+        id: 'toId',
+        value: toId
+      }).appendTo('#commentForm');
+    }
+    if ($('#commentId').length  > 0) {
+      $('#commentId').val(toId);
+    }else {
+      $('<input>').attr({
+        type: 'hidden',
+        name: 'comment[cid]',
+        id: 'commentID',
+        value: commentId,
+      }).appendTo('#commentForm')
+    }
   })
 });
